901bf7f9c3a94ba39a30a5dd80dd203b5d63ab94
Svetlana Mikhaylova <idillia8709@yandex.ru>
2021-09-23 10:37:45 +0300

    убран слеш в домене

--- configs/index.js
+++ configs/index.js
@@ -8,8 +8,8 @@ const CURRENT_PORT = NODE_ENV === 'production' && PORT ? PORT : 3001;
 const { BASE_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
 
 const ALLOWED_CORS = [
-  'https://home-page-student.nomoredomains.club/',
-  'http://home-page-student.nomoredomains.club/',
+  'https://home-page-student.nomoredomains.club',
+  'http://home-page-student.nomoredomains.club',
   'http://localhost:3000',
   'https://localhost:3000',
 ];
