5d5a8bf2d7fa18271debcedf96f6c49f5c05249a
Svetlana Mikhaylova <idillia8709@yandex.ru>
2021-09-23 10:46:06 +0300

    add cors

--- app.js
+++ app.js
@@ -3,15 +3,37 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const cors = require('cors');
 const router = require('./routes');
 const limiter = require('./utils/limiter');
 const { CURRENT_PORT, BASE_URL } = require('./configs/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const centrallError = require('./middlewares/central-errors');
-const corsOption = require('./middlewares/cors');
 
 const app = express();
+
+const allowedCors = [
+  'https://home-page-student.nomoredomains.club',
+  'http://home-page-student.nomoredomains.club',
+  'http://localhost:3000',
+  'https://localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  console.log('debug: ', origin);
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
 
 app.use(limiter);
 
@@ -27,8 +49,6 @@ mongoose.connect(BASE_URL, {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
-
-app.use(cors(corsOption));
 
 app.use(requestLogger);
 app.use(router);
