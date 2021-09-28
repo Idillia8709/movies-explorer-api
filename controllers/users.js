586d81395645769791a5658481afc50b2b9437af
Svetlana Mikhaylova <idillia8709@yandex.ru>
2021-09-29 00:45:10 +0300

    исправлен параметр sameSite в None

--- controllers/users.js
+++ controllers/users.js
@@ -93,7 +93,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('token', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: 'none',
+        sameSite: 'None',
         secure: true,
       })
         .send({ message: 'Вы успешно авторизовались' });
